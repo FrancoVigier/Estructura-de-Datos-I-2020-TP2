789adf2b36ede174f791c0294c1dff93a8898036
FrancoVigier <francovallejosvigier@gmail.com>
2020-06-04 18:28:19 -0300

    .

--- arbol_avl.h
+++ arbol_avl.h
@@ -13,17 +13,18 @@ struct ArbolAvl {
 
   struct ArbolAvl* derecha;
   struct ArbolAvl* izquierda;
+  int factor_balance;
 };
 
 typedef void Impresion(struct Rango);
 
-struct ArbolAvl* itree_crear();
+struct ArbolAvl* itree_crear(struct Rango rango);
 
 void itree_destruir(struct ArbolAvl* tree);
 
-void itree_insertar(struct ArbolAvl *tree, struct Rango rango);
+struct ArbolAvl*  itree_insertar(struct ArbolAvl *tree, struct Rango rango);
 
-void itree_eliminar(struct ArbolAvl* tree, struct Rango rango);
+struct ArbolAvl*  itree_eliminar(struct ArbolAvl* tree, struct Rango rango);
 
 bool itree_intersectar(struct ArbolAvl* tree, struct Rango rango);
 
@@ -31,4 +32,21 @@ void itree_recorrer_dfs(struct ArbolAvl *arbol, Impresion impresion);
 
 void itree_recorrer_bfs(struct ArbolAvl *arbol, Impresion impresion);
 
+struct ArbolAvl * minValueNode(struct ArbolAvl* node);
+
+void inOrder(struct ArbolAvl *root);
+
+void recalcularMax(struct ArbolAvl* node);
+
+int getBalance(struct ArbolAvl *N);
+
+struct ArbolAvl *rotacion_izquierda(struct ArbolAvl *x);
+
+struct ArbolAvl* rotacion_derecha(struct ArbolAvl* y);
+
+int max(int a, int b);
+
+int balance (struct ArbolAvl* N);
+
 #endif //ESTRUCTURA_DE_DATOS_I_2020_TP2_ARBOL_AVL_H
+
